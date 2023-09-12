9f6f74e2dbf6eac97f962520d37cf8d01646281e
SuprunovM <144795321+SuprunovM@users.noreply.github.com>
2023-09-12 14:36:58 +0300

    Add files via upload
    calculator

---
+++ calculator.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a , b;
+	cout << "input a: ";
+	cin >> a;
+	cout << "unput b: ";
+	cin >> b;
+	cout << "\n";
+
+	cout << a << "+" << b << "=" << a + b << "\n";
+	cout << a << "-" << b << "=" << a - b << "\n";
+	cout << a << "*" << b << "=" << a * b << "\n";
+	cout << a << "/" << b << "=" << a / b << "\n";
+}
