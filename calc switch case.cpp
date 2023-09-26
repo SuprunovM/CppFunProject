708cec24b9761dffe0d7027dda9b2beb63720d7b
SuprunovM <144795321+SuprunovM@users.noreply.github.com>
2023-09-26 12:56:04 +0300

    new good calc

---
+++ calc switch case.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a, b;
+	char c;
+	cout << "input a: ";
+	cin >> a;
+	cout << "unput b: ";
+	cin >> b;
+	cout << "deystvie: ";
+	cin >> c;
+
+	switch (c) {
+	case '+':
+		cout << a + b;
+		break;
+
+	case '-':
+		cout << a - b;
+		break;
+
+	case '*':
+		cout << a * b;
+		break;
+
+	case '/':
+		if (b == 0)
+			cout << "division by zero";
+		else
+			cout << a / b;
+		break;
+
+	default:
+		cout << "oshibka";
+		break;
+	}
+}
