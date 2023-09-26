96259a9c9026e3475490ffcad2b4dbfa87e62de3
SuprunovM <144795321+SuprunovM@users.noreply.github.com>
2023-09-26 14:42:35 +0300

    check reng

---
+++ check reng.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	float a, b;
+	cout << "Введите диапозон: ";
+	cin >> a >> b;
+	cout << a << " " << b;
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	cout << a << " " << b;
+	
+}
