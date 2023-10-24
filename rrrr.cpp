8e6fa6cea9a0c4024df558537d736aa1b4a0ca2c
SuprunovM <144795321+SuprunovM@users.noreply.github.com>
2023-10-24 12:20:49 +0300

    Add files via upload

---
+++ rrrr.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	setlocale(LC_ALL, " ");
+//	int a = 1;
+//	while (a <= 10) {
+//		cout << a++ << " ";
+//	}
+//
+//
+//}
+
+//int main() 
+//{
+//	setlocale(LC_ALL, "");
+//	int a, b, c=0;
+//	cin >> a >> b;
+//	if (a > b) swap(a, b);
+//	while (c >= a && c <= b)
+//		cout << c++ << " ";
+//	cout << c++;
+//}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int a;
+	cin >> a;
+	while (true) {
+	a != 0;
+	cout << a;
+}
+}
