c199aa07a30042bfc3a59897c27b94d32ee2f990
SuprunovM <144795321+SuprunovM@users.noreply.github.com>
2023-11-21 14:12:22 +0300

    Add files via upload

---
+++ leesen 9.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, " ");
+	srand(time(NULL));
+	int array[10];
+	int a = 1, b = 9;
+	for (int i = 0; i < 10; i++) {
+		array[i] = a + rand() % (b + 1 - a);
+		cout << array[i] << " ";
+	}
+	int n = 10;
+
+	for (int i = 0, flag; i < n; i++) {
+		flag = true;
+		for (int j = 0; j < n; j++) {
+			if (array[j] < array[j + 1]) {
+				swap(array[j], array[j + 1]);
+				flag = false;
+			}
+		}
+		if (flag) break;
+	}
+	cout << endl;
+}
